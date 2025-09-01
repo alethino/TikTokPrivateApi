84c2f3f52da0913d840074dbbbb4000233170711
neo galaxy <gneo0194@gmail.com>
2025-09-01 18:24:45 +0300

    Update lib.py

--- lib.py
+++ lib.py
@@ -9,7 +9,6 @@ mass registration of tik tok accounts, tik tok likes, comments, tik tok latest v
 mass registration of tik tok accounts, tik tok likes, comments, tik tok latest version
 
 ## tiktok API
-
 1. login to the user by email and username
 2. register new device
 3. receive information of the user through user_id
